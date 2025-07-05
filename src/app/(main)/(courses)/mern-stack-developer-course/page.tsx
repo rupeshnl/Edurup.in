408650980a784bd62cc220de67ac534755e8e4e8
jithendrabathala <jithendrabathala@gmail.com>
2025-07-05 11:48:52 +0530

    remove: tools section

--- src/app/(main)/(courses)/mern-stack-developer-course/page.tsx
+++ src/app/(main)/(courses)/mern-stack-developer-course/page.tsx
@@ -36,7 +36,7 @@ const page = () => {
         <SkillsSection />
 
         {/* Tools */}
-        <ToolsSection />
+        {/* <ToolsSection /> */}
 
         {/* Projects */}
         <ProjectsSection />
