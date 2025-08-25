d556e5eab861eb6f1dbc6a4237d0d125fe37a96b
jithendrabathala <jithendrabathala@gmail.com>
2025-08-25 19:36:35 +0530

    revert back

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,22 +1,21 @@
-// import Banner from "./components/Banner";
-// import News from "./components/News";
-// import Company from "./components/Company";
-// import Faculty from "./components/Faculty";
-// import FeaturedCourses from "./components/FeaturedCourses";
-// import Footer from "./components/Footer";
-// import Outcomes from "./components/Outcome";
+import Banner from "./components/Banner";
+import News from "./components/News";
+import Company from "./components/Company";
+import Faculty from "./components/Faculty";
+import FeaturedCourses from "./components/FeaturedCourses";
+import Footer from "./components/Footer";
+import Outcomes from "./components/Outcome";
 
 export default function Home() {
   return (
     <>
       <div className="min-h-full flex flex-col">
-        {/* <Banner />
+        <Banner />
         <Company />
         <FeaturedCourses />
         <Faculty />
         <Outcomes />
-        <News /> */}
-        <h1>Hello</h1>
+        <News />
       </div>
     </>
   );
