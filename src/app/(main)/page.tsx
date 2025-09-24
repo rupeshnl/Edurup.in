9c565d254c7bcd1516628dd65086f42829ac34fe
jithendrabathala <jithendrabathala@gmail.com>
2025-09-24 11:35:08 +0530

    made site live

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <div className="min-h-full flex flex-col">
-        <h1>Hello, world</h1>
+        {/* <h1>Hello, world</h1> */}
         <Banner />
         <Company />
         <FeaturedCourses />
