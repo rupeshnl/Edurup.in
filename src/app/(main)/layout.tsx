d9f8ea328010d8bd8f5b1eda4ccdd79abe769b8d
jithendrabathala <jithendrabathala@gmail.com>
2025-09-27 20:19:04 +0530

    ...

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,9 +4,9 @@ import Footer from "./components/Footer";
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="min-h-full dark:bg-dark">
-      <Navbar />
+      {/* <Navbar /> */}
       <main className="h-full mt-[94px] dark:bg-dark">{children}</main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -10,13 +10,13 @@ export default function Home() {
   return (
     <>
       <div className="min-h-full flex flex-col">
-        {/* <h1>Hello, world</h1> */}
-        <Banner />
+        <h1>Hello, world</h1>
+        {/* <Banner />
         <Company />
         <FeaturedCourses />
         <Faculty />
         <Outcomes />
-        <News />
+        <News /> */}
       </div>
     </>
   );
